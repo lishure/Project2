6455053f92a7e88b2c397e44fc0b79314c24dbb3
Lishure <HUEY_LISA@YAHOO.COM>
2019-06-05 21:25:36 -0700

    updated database and added node function

--- app/Agent/buyerdata.js
+++ app/Agent/buyerdata.js
@@ -2,7 +2,7 @@
 var mysql = require('mysql')
 // add inquirer
 var inquirer = require('inquirer')
-// create the mysql connection configuration for the bamazon database
+// create the mysql connection configuration
 var connection = mysql.createConnection({
   host: 'localhost',
   // Your port; if not 3306
@@ -13,42 +13,41 @@ var connection = mysql.createConnection({
   password: '',
   database: 'buyersdb'
 })
-// connect to the bamazon database
+// connect to buyersdb database
 connection.connect(function (err) {
   if (err) throw err
   start()
 })
-// function which prompts the user for what action they should take
+// function that asks the agent what to do
 function start () {
   inquirer
     .prompt({
       name: 'options',
       type: 'list',
       message: 'Would would you like to do?',
-      choices: ['Show All Clients', 'Show Clients By Purchase Price', 'Show Clients By Credit Score Range']
+      choices: ['Show All Clients', 'Show Clients By Credit Score Range', , 'Show Clients By Purchase Price']
     })
     .then(function (answer) {
       // based on their answer, run respective functions
       if (answer.options === 'Show All Clients') {
         showClients()
-      } else if (answer.options === 'Show Clients By Purchase Price') {
-        selectPrice()
       } else if (answer.options === 'Show Clients By Credit Score Range') {
         selectCredit()
+      } else if (answer.options === 'Show Clients By Purchase Price') {
+        selectPrice()
       }
     })
 }
-// function to view table of products
+// function to view all clients in database
 function showClients () {
-  connection.query('SELECT id,clientname,income,cashdown,creditgrade FROM clients', function (err, res) {
+  connection.query('SELECT * FROM clients', function (err, res) {
     if (err) throw err
     console.table(res)
     connection.end()
   })
 }
-// function to view stock quantity under than 5 units
+// function for prompt to select credit range
 function selectCredit () {
-/// ///////////////////////////////////////////////////
   inquirer
     .prompt({
       name: 'options',
@@ -70,112 +69,63 @@ function selectCredit () {
         aCredit()
       }
     })
-
+  // function to display ckients with F credit
   function fCredit () {
-    connection.query('SELECT id,clientname,income,cashdown FROM clients WHERE creditgrade = "F"', function (err, res) {
+    connection.query('SELECT * FROM clients WHERE creditgrade = "F"', function (err, res) {
       if (err) throw err
       console.table(res)
       connection.end()
     })
   }
-
+  // function to display ckients with d credit
   function dCredit () {
-    connection.query('SELECT id,clientname,income,cashdown FROM clients WHERE creditgrade = "D"', function (err, res) {
+    connection.query('SELECT * FROM clients WHERE creditgrade = "D"', function (err, res) {
       if (err) throw err
       console.table(res)
       connection.end()
     })
   }
-
+  // function to display ckients with c credit
   function cCredit () {
-    connection.query('SELECT id,clientname,income,cashdown FROM clients WHERE creditgrade = "C"', function (err, res) {
+    connection.query('SELECT * FROM clients WHERE creditgrade = "C"', function (err, res) {
       if (err) throw err
       console.table(res)
       connection.end()
     })
   }
-
+  // function to display ckients with b credit
   function bCredit () {
-    connection.query('SELECT id,clientname,income,cashdown FROM clients WHERE creditgrade = "B"', function (err, res) {
+    connection.query('SELECT * FROM clients WHERE creditgrade = "B"', function (err, res) {
       if (err) throw err
       console.table(res)
       connection.end()
     })
   }
-
+  // function to display clients with a credit
   function aCredit () {
-    connection.query('SELECT id,clientname,income,cashdown FROM clients WHERE creditgrade = "A"', function (err, res) {
+    connection.query('SELECT * FROM clients WHERE creditgrade = "A"', function (err, res) {
       if (err) throw err
       console.table(res)
       connection.end()
     })
   }
-  /// /////////////////////////////////////////////////////////
-
-//   var query = 'SELECT sku,product_name,stock_quantity FROM products WHERE stock_quantity < 5'
-//   connection.query(query, function (err, res) {
-//     if (err) throw err
-//     console.table(res)
-  // connection.end()
 }
-
-// function to replenish inventory
+// function to display clients by budget
+/// /////////////////////////////////////////////////////////
+/// /////////////////////////////////////////////////////////////////////////////////
+// function code is not finished yet
 function selectPrice () {
   inquirer.prompt([
     {
-      name: 'needSku',
-      message: 'What is the SKU of the product you wish to replenish?'
-    }, {
-      name: 'quantity',
-      message: 'How many units would you like to add?'
+      name: 'budget',
+      message: 'What is the max budget?'
     }
   ]).then(function (answers) {
-    connection.query('SELECT stock_quantity,product_name FROM products WHERE sku=?', answers.needSku, function (err, results) {
+    connection.query('SELECT * FROM clients WHERE budget=?', answers.needBudget, function (err, results) {
       if (err) { console.log(err) };
-      // add user input units to current stock quantity
-      var replenish = parseInt(results[0].stock_quantity) + parseInt(answers.quantity)
-      var pName = results[0].product_name
-      // updates stock quantity of product matching sku number
-      connection.query('UPDATE products SET stock_quantity=? WHERE sku=?', [replenish, answers.needSku],
-        function (err, results) {
-          if (err) { console.log(err) };
-          console.log(answers.quantity + ' items added to ' + pName)
-          showClients()
-        })
+      /// Code is not finished yet
     })
   })
 }
-// function to add new product
-function addNew () {
-  // Prompt the user to enter information about the new product
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'product_name',
-      message: 'Please enter the name of the product you wish to add:'
-    },
-    {
-      type: 'input',
-      name: 'department_name',
-      message: 'Which department does the new product belong to?'
-    },
-    {
-      type: 'input',
-      name: 'price',
-      message: 'What is the price per item?'
-    },
-    {
-      type: 'input',
-      name: 'stock_quantity',
-      message: 'How many items are being added?'
-    }
-  ]).then(function (input) {
-    // Add new product to bamazon database
-    connection.query('INSERT INTO products SET ?', input, function (error, results, fields) {
-      if (error) throw error
-      console.log('New product has been added under SKU # ' + results.insertId + '.')
-      // shows updated current items available
-      showClients()
-    })
-  })
-}
+/// ////////////////// ///Code is not finished yet
+/// /////////////////////////////////////////
