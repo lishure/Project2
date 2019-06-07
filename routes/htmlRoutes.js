deaee449966d21b55468d3ee9f91f6060d5a269a
cuongle30 <cuongle30@gmail.com>
2019-06-06 20:46:40 -0700

    update htmlroutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,36 +1,12 @@
-// Dependecies
-const Example = require('../models/example')
+//Creating path when user visit the page
+var path = require('path')
 
-/**
- * htmlRoutes: This routes file renders views e.g. handlebars pages
- * It differs from the apiRoutes.js file in that it responds to the client/view requests with a
- * handlebars page where the apiRoutes.js responds with data onlu
- *
- */
-module.exports = function (app) {
-  // Load index page
-  app.get('/', function (req, res) {
-    Example.findAll({})
-      .then(function (dbExamples) {
-        res.render('index', {
-          msg: 'Welcome!',
-          examples: dbExamples
-        })
-      })
-  })
+module.exports = function (app){
+    app.get('', function (req, res){
+        res.sendFile(path.join(__dirname, '../public/home.html'))
+    });
 
-  // Load example page and pass in an example by id
-  app.get('/example/:id', function (req, res) {
-    Example.findOne({ where: { id: req.params.id } })
-      .then(function (dbExample) {
-        res.render('example', {
-          example: dbExample
-        })
-      })
-  })
-
-  // Render 404 page for any unmatched routes
-  app.get('*', function (req, res) {
-    res.render('404')
-  })
+    app.get('/survey', function (req, res){
+        res.sendFile(path.join(__dirname, '../public/survey-form.html'))
+    });
 }
