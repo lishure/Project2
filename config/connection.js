d1accdef3da578c38fbd33ceb5d759c1dcfb97ad
cuongle30 <cuongle30@gmail.com>
2019-06-08 20:39:23 -0700

    display sql data in html

--- config/connection.js
+++ config/connection.js
@@ -14,3 +14,24 @@ const Knex = require('knex')(require('../knexfile')[ENV])
 
 // Exports the connection for other files to use
 module.exports = Knex
+
+// Set up our connection information
+var connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'nodeuser',
+    password: '',
+    database: 'buyersdb'
+  })
+  
+  // Connect to the database
+  connection.connect(function (err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack)
+      return;
+    }
+    console.log('connected as id ' + connection.threadId)
+  })
+  
+  // Export connection
+  module.exports = connection
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,7 +10,7 @@ const Example = require('../models/example')
 
 module.exports = function (app) {
   // Get all examples
-  app.get('/api/examples', function (req, res) {
+  app.get('/api/all-entries', function (req, res) {
     Example.findAll()
       .then(function (dbExamples) {
         res.json(dbExamples)
@@ -33,3 +33,33 @@ module.exports = function (app) {
       })
   })
 }
+
+var connection = require('../config/connection.js')
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+  // Get all chirps
+  app.get('/all-entries', function (req, res) {
+    var dbQuery = 'SELECT * FROM clients';
+
+    connection.query(dbQuery, function (err, result) {
+      if (err) throw err
+      res.json(result)
+    })
+  })
+
+  // // Add a chirp
+  // app.post('/api/new', function (req, res) {
+  //   console.log('Chirp Data:')
+  //   console.log(req.body)
+
+  //   var dbQuery = 'INSERT INTO chirps (author, body, created_at) VALUES (?,?,?)';
+
+  //   connection.query(dbQuery, [req.body.author, req.body.body, req.body.created_at], function (err, result) {
+  //     if (err) throw err
+  //     console.log('Chirp Successfully Saved!')
+  //     res.end()
+  //   })
+  // })
+};
