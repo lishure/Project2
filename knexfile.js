24e9a0f49a2fbbcd0a8e41881e29f5ec3d7746da
Dru Kelly <drukelly@gmail.com>
2019-06-14 15:04:02 -0700

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -27,7 +27,6 @@ module.exports = {
       user: process.env.USER,
       password: process.env.PASSWORD,
       host: process.env.HOST,
-      port: 3306,
       database: process.env.DATABASE
     },
     migrations: {
