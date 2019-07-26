0fbc69790d3a0b421c3b02408b26e501f89683f6
cuongle30 <cuongle30@gmail.com>
2019-07-25 20:32:07 -0700

    update

--- knex/migrations/20190608095713_create_buyers_table.js
+++ knex/migrations/20190608095713_create_buyers_table.js
@@ -1,18 +1,3 @@
-
-
-// exports.up = function (knex, Promise) { 
-//   knex.schema.hasTable('buyers')
-//   return knex.schema.createTable('buyers', function (table) {
-//     table.increments('id')
-//     table.string('clientname').notNullable()
-//     table.integer('income').notNullable()
-//     table.integer('cashdown')
-//     table.integer('budget')
-//     table.string('creditgrade').notNullable()
-//     table.string('commentsection')
-//     table.timestamp('created_at').defaultTo(knex.fn.now())
-//   })
-// }
 exports.up = function (knex, Promise) { 
   knex.schema.hasTable('buyers')
   return knex.schema.createTable('buyers', function (table) {
@@ -25,43 +10,7 @@ exports.up = function (knex, Promise) {
     table.string('commentsection')
     table.timestamp('created_at').defaultTo(knex.fn.now())
   })
-// exports.down = function (knex, Promise) {
-//   return knex.schema.dropTable('buyers'),
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('buyers'),
 
-<<<<<<< Updated upstream
-// }
-
-exports.up = function(knex, Promise) {
-  return Promise.all([
-
-    knex.schema.createTable('buyers', function (table) {
-      table.increments('id')
-      table.string('clientname').notNullable()
-      table.integer('income').notNullable()
-      table.integer('cashdown')
-      table.integer('budget')
-      table.string('creditgrade').notNullable()
-      table.string('commentsection')
-      table.timestamp('created_at').defaultTo(knex.fn.now())
-    }),
-    knex.schema.createTable('chat', function (table) {
-      table.increments('id')
-      table.string('email').notNullable()
-      table.string('msg').notNullable()
-      table.timestamp('created_at').defaultTo(knex.fn.now())
-    })
-
-  ])
-=======
->>>>>>> Stashed changes
-}
-
-exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.dropTable('buyers'),
-<<<<<<< Updated upstream
-    knex.schema.dropTable('chat')
-  ])
-=======
->>>>>>> Stashed changes
 }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,6 @@
 // eslint-disable-next-line no-useless-catch
 // Dependencies
 const Buyers = require('../models/buyers')
-const Chat = require('../models/chat')
 /**
  * apiRoutes: This routes file returns data to the client/view
  * It differs from the htmlRoutes.js file in that it responds to the client/view requests with data
@@ -10,46 +9,6 @@ const Chat = require('../models/chat')
  */
 
 module.exports = function (app) {
-  // Get all chat
-  app.get('/api/chat', (req, res) => {
-    Chat.findAll()
-      .then(chat => {
-        res.json(chat)
-      })
-  })
-
-  // Get chat by id
-  app.get('/api/chat/:id', (req, res) => {
-    Chat.getID(req.params)
-      .then(chat => {
-        res.json(chat)
-      })
-  })
-
-  // Create a new chat
-  app.post('/api/chat', (req, res) => {
-    Chat.create(req.body)
-      .then(chat => {
-        res.json(chat)
-      })
-  })
-
-  // Delete an chat by id
-  app.delete('/api/chat/:id', (req, res) => {
-    Chat.destroy(req.params)
-      .then(chat => {
-        res.json(chat)
-      })
-  })
-
-  // Update an chat by id
-  app.put('/api/chat/:id', (req, res) => {
-    Chat.update(req.params, req.body)
-      .then(chat => {
-        res.json(chat)
-      })
-  })
-
   // Get all buyers
   app.get('/api/buyers', (req, res) => {
     Buyers.findAll()
