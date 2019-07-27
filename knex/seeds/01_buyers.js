2a1472f18dd47b65e3591125159a58066539e06e
Lishure <HUEY_LISA@YAHOO.COM>
2019-07-26 22:51:56 -0700

    defined comment

--- knex/seeds/01_buyers.js
+++ knex/seeds/01_buyers.js
@@ -5,18 +5,18 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('buyers').insert([
-        { clientname: 'Joey', income: 5000, cashdown: 50000, budget: 400000, creditgrade: 'Good' },
-        { clientname: 'Rachael', income: 6000, cashdown: 80000, budget: 600000, creditgrade: 'Very Good' },
-        { clientname: 'Ross', income: 7000, cashdown: 100000, budget: 800000, creditgrade: 'Exceptional' },
-        { clientname: 'Lucky', income: 5500, cashdown: 50000, budget: 400000, creditgrade: 'Exceptional' },
-        { clientname: 'Prince', income: 6500, cashdown: 120000, budget: 600000, creditgrade: 'Fair' },
-        { clientname: 'Fluffy', income: 8000, cashdown: 100000, budget: 900000, creditgrade: 'Exceptional' },
-        { clientname: 'Rabbi', income: 8900, cashdown: 70000, budget: 400000, creditgrade: 'Good' },
-        { clientname: 'Fatboy', income: 3200, cashdown: 95000, budget: 300000, creditgrade: 'Very Poor' },
-        { clientname: 'Oldman', income: 4800, cashdown: 100000, budget: 500000, creditgrade: 'Very Poor' },
-        { clientname: 'Cartman', income: 3800, cashdown: 50000, budget: 300000, creditgrade: 'Good' },
-        { clientname: 'Randy', income: 4200, cashdown: 95000, budget: 500000, creditgrade: 'Very Poor' },
-        { clientname: 'Butters', income: 5800, cashdown: 100000, budget: 600000, creditgrade: 'Very Poor' }
+        { clientname: 'Joey', income: 5000, cashdown: 50000, budget: 400000, creditgrade: 'Good', commentsection: 'none0'},
+        { clientname: 'Rachael', income: 6000, cashdown: 80000, budget: 600000, creditgrade: 'Very Good', commentsection: 'none'},
+        { clientname: 'Ross', income: 7000, cashdown: 100000, budget: 800000, creditgrade: 'Exceptional', commentsection: 'none'},
+        { clientname: 'Lucky', income: 5500, cashdown: 50000, budget: 400000, creditgrade: 'Exceptional', commentsection: 'none'},
+        { clientname: 'Prince', income: 6500, cashdown: 120000, budget: 600000, creditgrade: 'Fair', commentsection: 'none'},
+        { clientname: 'Fluffy', income: 8000, cashdown: 100000, budget: 900000, creditgrade: 'Exceptional', commentsection: 'none'},
+        { clientname: 'Rabbi', income: 8900, cashdown: 70000, budget: 400000, creditgrade: 'Good', commentsection: 'none'},
+        { clientname: 'Fatboy', income: 3200, cashdown: 95000, budget: 300000, creditgrade: 'Very Poor', commentsection: 'none'},
+        { clientname: 'Oldman', income: 4800, cashdown: 100000, budget: 500000, creditgrade: 'Very Poor', commentsection: 'none'},
+        { clientname: 'Cartman', income: 3800, cashdown: 50000, budget: 300000, creditgrade: 'Good', commentsection: 'none'},
+        { clientname: 'Randy', income: 4200, cashdown: 95000, budget: 500000, creditgrade: 'Very Poor', commentsection: 'none'},
+        { clientname: 'Butters', income: 5800, cashdown: 100000, budget: 600000, creditgrade: 'Very Poor', commentsection: 'none'}
       ])
     })
 }
--- models/seed.sql
+++ models/seed.sql
@@ -1,37 +1,37 @@
 USE buyersdb;
 
 INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade)
-VALUES ("Joey", 5000, 50000, 400000, "Good");
+VALUES ("Joey", 5000, 50000, 400000, "Good", "None");
 
-INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade)
-VALUES ("Rachael", 6000, 80000, 600000, "Very Good");
+INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade, commentsection)
+VALUES ("Rachael", 6000, 80000, 600000, "Very Good", "None");
 
-INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade)
-VALUES ("Ross", 7000, 100000, 800000, "Exceptional");
+INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade, commentsection)
+VALUES ("Ross", 7000, 100000, 800000, "Exceptional", "None");
 
-INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade)
-VALUES ("Lucky", 5500, 50000, 400000, "Exceptional");
+INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade, commentsection)
+VALUES ("Lucky", 5500, 50000, 400000, "Exceptional", "None");
 
-INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade)
-VALUES ("Prince", 6500, 120000, 600000, "Fair");
+INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade, commentsection)
+VALUES ("Prince", 6500, 120000, 600000, "Fair", "None");
 
-INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade)
-VALUES ("Fluffy", 8000, 100000, 900000, "Exceptional");
+INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade, commentsection)
+VALUES ("Fluffy", 8000, 100000, 900000, "Exceptional", "None");
 
-INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade)
-VALUES ("Rabbi", 8900, 70000, 400000, "Very Good");
+INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade, commentsection)
+VALUES ("Rabbi", 8900, 70000, 400000, "Very Good", "None");
 
-INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade)
-VALUES ("Fatboy", 3200, 95000, 300000, "Very Poor");
+INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade, commentsection)
+VALUES ("Fatboy", 3200, 95000, 300000, "Very Poor", "None");
 
-INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade)
-VALUES ("Oldman", 4800, 100000, 500000, "Very Poor");
+INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade, commentsection)
+VALUES ("Oldman", 4800, 100000, 500000, "Very Poor", "None");
 
-INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade)
-VALUES ("Cartman", 3800, 50000, 300000, "Very Good");
+INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade, commentsection)
+VALUES ("Cartman", 3800, 50000, 300000, "Very Good", "None");
 
-INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade)
-VALUES ("Randy", 4200, 95000, 500000, "Very Poor");
+INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade, commentsection)
+VALUES ("Randy", 4200, 95000, 500000, "Very Poor", "None");
 
-INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade)
-VALUES ("Butters", 5800, 100000, 600000, "Very Poor");
+INSERT INTO buyers (clientname, income, cashdown, budget, creditgrade, commentsection)
+VALUES ("Butters", 5800, 100000, 600000, "Very Poor", "None");
