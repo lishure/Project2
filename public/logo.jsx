59ea8de7c665b50d79c4ef7154c0a4739c981330
cuongle30 <cuongle30@gmail.com>
2019-08-02 17:49:32 -0700

    add react

---
+++ public/logo.jsx
@@ -0,0 +1,6 @@
+var rootEl = document.getElementById('root')
+var element = React.createElement('img', {
+  src: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+  id: 'header'
+})
+ReactDOM.render(element, rootEl)
