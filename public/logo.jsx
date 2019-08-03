44ebecbf083b6690844523b354dece21d7162676
cuongle30 <cuongle30@gmail.com>
2019-08-02 18:11:49 -0700

    update logo

--- public/logo.jsx
+++ public/logo.jsx
@@ -1,6 +1,6 @@
 var rootEl = document.getElementById('root')
 var element = React.createElement('img', {
-  src: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+  src: './images/android-chrome-512x512.png',
   id: 'header'
 })
 ReactDOM.render(element, rootEl)
