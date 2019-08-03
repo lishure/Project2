c38da2b52d93a4616298955ae5fd48620b48f030
Roberto Castanon <robertocastanon925@gmail.com>
2019-08-03 12:44:37 -0700

    Added chatbot, removed home.html changed / route in html route and cleaned up uneeded comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = app => {
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/home.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
   })
 
   app.get('/survey', (req, res) => {
